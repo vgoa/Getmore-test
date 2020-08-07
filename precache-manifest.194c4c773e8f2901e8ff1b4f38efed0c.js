db0f25cc5bc7e051363a37587fad72d637e810ce
VICTOR GERARDO <victorgerardo@MacBook-Pro-de-Victor.local>
2020-08-07 09:13:57 -0500

    Updates

---
+++ precache-manifest.194c4c773e8f2901e8ff1b4f38efed0c.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc72a77938e23095b1787af73893f753",
+    "url": "/getmore-test/index.html"
+  },
+  {
+    "revision": "1024ad8402d041699a87",
+    "url": "/getmore-test/static/css/main.5c317d12.chunk.css"
+  },
+  {
+    "revision": "90b9c2a9266a58969eff",
+    "url": "/getmore-test/static/js/2.f17ebd08.chunk.js"
+  },
+  {
+    "revision": "8bc88370bc2489d86f2f357f447ddb0b",
+    "url": "/getmore-test/static/js/2.f17ebd08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1024ad8402d041699a87",
+    "url": "/getmore-test/static/js/main.c78190bf.chunk.js"
+  },
+  {
+    "revision": "9280eef71b92de249c82",
+    "url": "/getmore-test/static/js/runtime-main.8f1e74de.js"
+  },
+  {
+    "revision": "96272d7350f7816a001e6a52276a6c9c",
+    "url": "/getmore-test/static/media/action-athlete-ball-342361.96272d73.jpg"
+  },
+  {
+    "revision": "f53fa4867305f9f3a7e7e8993dba2bc8",
+    "url": "/getmore-test/static/media/action-athlete-ball-342361@2x.f53fa486.jpg"
+  },
+  {
+    "revision": "c30a1c1b299c59e7170f771ef3a6be7d",
+    "url": "/getmore-test/static/media/action-athlete-ball-342361@3x.c30a1c1b.jpg"
+  },
+  {
+    "revision": "27c3d0b969d7b7ac019ac3e4822cf38d",
+    "url": "/getmore-test/static/media/ball-color-court-1405355.27c3d0b9.png"
+  },
+  {
+    "revision": "c2b78d8020d626c98e875069acd93eef",
+    "url": "/getmore-test/static/media/ball-color-court-1405355@2x.c2b78d80.png"
+  },
+  {
+    "revision": "7ae437141b6df3b3445f2709f6da12a8",
+    "url": "/getmore-test/static/media/group-11.7ae43714.png"
+  },
+  {
+    "revision": "2df2f11cccda9acdeadeeccdc6f00731",
+    "url": "/getmore-test/static/media/group-11@2x.2df2f11c.png"
+  },
+  {
+    "revision": "7dcde5d26829b673bdde3528cf8083e8",
+    "url": "/getmore-test/static/media/group-11@3x.7dcde5d2.png"
+  },
+  {
+    "revision": "dade1fd8e136dcb28e819d3d1e35220a",
+    "url": "/getmore-test/static/media/group.dade1fd8.png"
+  },
+  {
+    "revision": "b16b6f4a1f8a175bf52cb2e07c2e6170",
+    "url": "/getmore-test/static/media/group@2x.b16b6f4a.png"
+  }
+]);
